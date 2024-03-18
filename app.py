2f132aae976e5709531a3c9207f51769fc458800
Emdinga <emdinga@gmail.com>
2024-03-18 02:22:12 -0700

    adding docker file

--- app.py
+++ app.py
@@ -12,4 +12,6 @@ def index():
     return render_template('frontend/static', 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
