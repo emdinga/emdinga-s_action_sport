342e44e88e4ded948371b7d113c82ad9ba902a07
Emdinga <emdinga@gmail.com>
2024-03-18 12:03:07 -0700

    login page

--- app.py
+++ app.py
@@ -11,6 +11,22 @@ def index():
     """root of the project"""
     return render_template('index.html')
 
+@app.route('/api/login', methods=['POST'])
+def login_user():
+    """Handle POST request to authenticate user login"""
+    """"Extract login credentials from request"""
+    data = request.json
+    cell_number = data.get('cell_number')
+    """Query database to retrieve user data by cell number"""
+    db.cursor.execute('''
+        SELECT * FROM User WHERE cell_number = ?
+    ''', (cell_number,))
+    user = db.cursor.fetchone()
+    if user:
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"message": "Login failed"}), 401
+
 if __name__ == '__main__':
     with app.app_context():
         app.run(host='0.0.0.0', port=5000, debug=True)
