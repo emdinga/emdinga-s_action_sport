593a7f14c587ef95197ebf011a0880fba1fd235e
Emdinga <emdinga@gmail.com>
2024-03-18 12:37:06 -0700

    fixing login page

--- app.py
+++ app.py
@@ -11,21 +11,13 @@ def index():
     """root of the project"""
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login_user():
-    """Handle POST request to authenticate user login"""
-    """"Extract login credentials from request"""
-    data = request.json
-    cell_number = data.get('cell_number')
-    """Query database to retrieve user data by cell number"""
-    db.cursor.execute('''
-        SELECT * FROM User WHERE cell_number = ?
-    ''', (cell_number,))
-    user = db.cursor.fetchone()
-    if user:
-        return jsonify({"message": "Login successful"}), 200
-    else:
-        return jsonify({"message": "Login failed"}), 401
+from flask import render_template
+
+@app.route('/login')
+def login_page():
+    """Render the login page"""
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     with app.app_context():
