9408e7774b1ed31308116399135ae0a390bc3d4d
Emdinga <emdinga@gmail.com>
2024-03-18 07:21:38 -0700

    testing js

--- templates/map.js
+++ templates/map.js
@@ -1,11 +1,17 @@
+// Define the API key
+const apiKey = 'AIzaSyAfWvp0dEAr6KzE4QXll0O82fOlfqkWXNk';
+
+// Function to initialize and add the map
 function initMap() {
     // The location of Football on Court Africa Durban
     var location = { lat: -29.820594, lng: 31.032515 };
+
     // The map, centered at Football on Court Africa Durban
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: location,
     });
+
     // The marker, positioned at Football on Court Africa Durban
     var marker = new google.maps.Marker({
         position: location,
@@ -13,3 +19,7 @@ function initMap() {
     });
 }
 
+// Call the initMap function when the window loads
+window.onload = function () {
+    initMap();
+};
