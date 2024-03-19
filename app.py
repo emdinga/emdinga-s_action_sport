4c0d806a603ddeb77ba805b6a98db4b007e7c7de
Emdinga <emdinga@gmail.com>
2024-03-19 11:51:57 -0700

    Flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """entry"""
 
-from flask import Flask, render_template,request, jsonify, session, g
+from flask import Flask, render_template,request, jsonify, session, g, redirect
 from database.database import Database
 import secrets, hashlib, bcrypt, sqlite3
 
